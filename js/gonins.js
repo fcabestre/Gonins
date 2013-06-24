93eaff9cf217abcfdda548fdc7f583b5b5a006bf
Frédéric Cabestre <frederic.cabestre@free.fr>
2013-06-24 09:26:38 +0200

    Proposition de deux types de galeries différentes : un carousel et un ensemble de vignettes.

--- js/gonins.js
+++ js/gonins.js
@@ -1,7 +1,5 @@
 function ListeDesMatchs($scope, $http) {
-    $http.get('data/matchs.json').success(function(data, status) {
-	$scope.matchs = data;
-    }).error(function(data, status){
-	/* Todo */
+    $http.get('data/matchs.json').success(function (data, status) {
+        $scope.matchs = data;
     });
 }
