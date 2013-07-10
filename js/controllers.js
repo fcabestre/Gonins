d3cbd64c9ad469dc0f7e0a0ce8dcfdb651165e95
Frédéric Cabestre <frederic.cabestre@free.fr>
2013-07-10 19:29:06 +0200

    Réécriture des contrôleurs en tant qu'éléments du module AngularJS global. A noter que dans la déclaration des routes, ces contrôleurs apparraissent comme des chaînes de caractère et non comme des symboles !

--- js/controllers.js
+++ js/controllers.js
@@ -1,20 +1,23 @@
-function GoninsCtrl($scope, $http) {
-}
+var controllers = {};
 
-function CalendrierCtrl($scope, $http) {
+controllers.GoninsCtrl = function ($scope) {
+};
+
+controllers.CalendrierCtrl = function ($scope, $http) {
     $http.get('data/matchs.json').success(function (data, status) {
         $scope.matchs = data;
     });
-}
+};
 
-function GalerieCtrl($scope, $http) {
-}
+controllers.GalerieCtrl = function ($scope) {
+};
 
-function ContactCtrl($scope, $window) {
+controllers.ContactCtrl = function ($scope, $window) {
     $scope.zoom = function() {
         $window.location.href = 'http://www.openstreetmap.org/?lat=43.573735&amp;lon=1.48414&amp;zoom=17&amp;layers=M&amp;mlat=43.57404&amp;mlon=1.48519'
     };
-}
+};
 
+goninsModule.controller(controllers);
 
 
--- js/routes.js
+++ js/routes.js
@@ -2,9 +2,9 @@ var goninsModule = angular.module('gonins', []);
 
 goninsModule.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
-        when('/gonins', {templateUrl:'partials/gonins.html', controller:GoninsCtrl}).
-        when('/calendrier', {templateUrl:'partials/calendrier.html', controller:CalendrierCtrl}).
-        when('/galerie', {templateUrl:'partials/galerie.html', controller:GalerieCtrl}).
-        when('/contact', {templateUrl:'partials/contact.html', controller:ContactCtrl}).
+        when('/gonins', {templateUrl:'partials/gonins.html', controller:'GoninsCtrl'}).
+        when('/calendrier', {templateUrl:'partials/calendrier.html', controller:'CalendrierCtrl'}).
+        when('/galerie', {templateUrl:'partials/galerie.html', controller:'GalerieCtrl'}).
+        when('/contact', {templateUrl:'partials/contact.html', controller:'ContactCtrl'}).
         otherwise({redirectTo:'/gonins'});
 }]);
