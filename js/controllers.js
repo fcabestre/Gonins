19007b3fe8c045d10e8fbc0d1db6d753b2edf18e
Frédéric Cabestre <frederic.cabestre@free.fr>
2013-07-18 22:57:12 +0200

    Surbrillance permanente de l'item de menu selectionné. Traité à travers le mécanisme de 'scope' d'AngularJS par le biais d'évènements remontés par les controleurs de vue lorsque celle-ci sont activées. Pas clair ? C'est sûr. Et pourtant c'est là qu'on voit la puisance d'AngularJS !

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,50 @@
 var controllers = {};
 
+controllers.NavbarCtrl = function ($scope) {
+    $scope.$on('gonins', function() {
+        $scope.gonins = 'active';
+        $scope.calendrier = '';
+        $scope.galerie = '';
+        $scope.contact = '';
+
+    });
+    $scope.$on('calendrier', function() {
+        $scope.gonins = '';
+        $scope.calendrier = 'active';
+        $scope.galerie = '';
+        $scope.contact = '';
+    });
+    $scope.$on('galerie', function() {
+        $scope.gonins = '';
+        $scope.calendrier = '';
+        $scope.galerie = 'active';
+        $scope.contact = '';
+    });
+    $scope.$on('contact', function() {
+        $scope.gonins = '';
+        $scope.calendrier = '';
+        $scope.galerie = '';
+        $scope.contact = 'active';
+    });
+};
+
 controllers.GoninsCtrl = function ($scope) {
+    $scope.$emit('gonins');
 };
 
 controllers.CalendrierCtrl = function ($scope, dataFactory) {
+    $scope.$emit('calendrier');
     dataFactory.getMatches(function(data) {
         $scope.matchs = data;
     });
 };
 
 controllers.GalerieCtrl = function ($scope) {
+    $scope.$emit('galerie');
 };
 
 controllers.ContactCtrl = function ($scope, $window) {
+    $scope.$emit('contact');
     $scope.zoom = function() {
         $window.location.href = 'http://www.openstreetmap.org/?lat=43.573735&amp;lon=1.48414&amp;zoom=17&amp;layers=M&amp;mlat=43.57404&amp;mlon=1.48519'
     };
