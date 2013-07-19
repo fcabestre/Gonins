973eb23733916fd775292751465caee80c3ccb57
Frédéric Cabestre <frederic.cabestre@free.fr>
2013-07-19 21:49:47 +0200

    Amélioration du mécanisme d'évènement et du javascript pour sa gestion

--- js/controllers.js
+++ js/controllers.js
@@ -1,50 +1,31 @@
 var controllers = {};
 
-controllers.NavbarCtrl = function ($scope) {
-    $scope.$on('gonins', function() {
-        $scope.gonins = 'active';
-        $scope.calendrier = '';
-        $scope.galerie = '';
-        $scope.contact = '';
-
-    });
-    $scope.$on('calendrier', function() {
-        $scope.gonins = '';
-        $scope.calendrier = 'active';
-        $scope.galerie = '';
-        $scope.contact = '';
-    });
-    $scope.$on('galerie', function() {
-        $scope.gonins = '';
-        $scope.calendrier = '';
-        $scope.galerie = 'active';
-        $scope.contact = '';
-    });
-    $scope.$on('contact', function() {
-        $scope.gonins = '';
-        $scope.calendrier = '';
-        $scope.galerie = '';
-        $scope.contact = 'active';
+controllers.NavbarCtrl = function ($scope, $log) {
+    $scope.$on('view', function(event, isActive) {
+        $log.info(isActive);
+        var tmp = ['','','',''];
+        tmp[isActive] = 'active';
+        $scope.isActive = tmp;
     });
 };
 
 controllers.GoninsCtrl = function ($scope) {
-    $scope.$emit('gonins');
+    $scope.$emit('view', 0);
 };
 
 controllers.CalendrierCtrl = function ($scope, dataFactory) {
-    $scope.$emit('calendrier');
+    $scope.$emit('view', 1);
     dataFactory.getMatches(function(data) {
         $scope.matchs = data;
     });
 };
 
 controllers.GalerieCtrl = function ($scope) {
-    $scope.$emit('galerie');
+    $scope.$emit('view', 2);
 };
 
 controllers.ContactCtrl = function ($scope, $window) {
-    $scope.$emit('contact');
+    $scope.$emit('view', 3);
     $scope.zoom = function() {
         $window.location.href = 'http://www.openstreetmap.org/?lat=43.573735&amp;lon=1.48414&amp;zoom=17&amp;layers=M&amp;mlat=43.57404&amp;mlon=1.48519'
     };
